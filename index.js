24d23cf51e5a2afe462577424c34cbf7c34cfd53
Liam Tinney <ltinney@powerhrg.com>
2020-07-19 11:11:27 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function superbowlWin(array) {
+  const superbowl = array.find(element => element.result === "W")
+  return superbowl ? superbowl.year : undefined
+}
